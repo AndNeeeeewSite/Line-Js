fdf3292ad270756aafe880ab1da8dfaf3e30d27c
AndNeeeeewSite <pavlopysarenko1234@gmail.com>
2024-07-25 15:10:46 +0300

    !

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//   1.
+result = 5+5+'5'
+console.log(typeof result)
+//   2.
+const email = 'pavlopysarenko123@gmail.com'
+haveDog = email.includes('@')
+length = email.length
+console.log('Тут є @ ? ' + haveDog + "Кількість символів: "+ length)
+//   3.
+my = 'My '
+name = 'name '
+is = 'is '
+fullName = my+name+is + 'Viktor'
+console.log(fullName)
+//   4.
+userName = 'Олександро'
+payment = '300 гривень'
+alert('Дякуємо, '+ userName + "! До сплати "+payment)
